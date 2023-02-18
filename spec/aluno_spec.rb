878017ab7bc9e14d6868d1a4932fd434cce0ad3e
matheusreirasi <matheus.reirasi@gmail.com>
2023-02-18 15:49:03 -0300

    pasta de execução de testes

--- spec/aluno_spec.rb
+++ spec/aluno_spec.rb
@@ -1,11 +1,31 @@
-require "aluno"
+# in spec/aluno_spec.rb
+require "./classes/aluno.rb"
+require "./classes/db.rb"
+
 RSpec.describe Aluno do
-    subject(:aluno) {false_class.new(bool)}
-    describe "#uffmail_disponivel" do
-        context "Uffmail está disponível?" do
-            let(:aluno) {false}
-            it "retorna a disponibilidade do Uffmail" do
-                expect(aluno.uffmail_disponivel).to eq(false)
+    db = DB.new("./database/lista.csv")
+    aluno = Aluno.new(db.busca("105226"))
+
+    describe "#initialize" do
+        context "Classe está recebendo um aluno?" do
+            it "retorna o nome se estiver funcinando" do
+                expect(aluno.nome).to eq("Caua Melo Alves")
+            end
+        end
+    end
+
+    describe "#possui_uffmail?" do
+        context "Aluno já possui UFFmail?" do
+            it "por padrão deve retornar false caso haja uma conta cadastrada" do
+                expect(aluno.possui_uffmail?).to eq(false)
+            end
+        end
+    end
+
+    describe "#aluno_status?" do
+        context "O aluno possui matrícula ativa?" do
+            it "retorna true se o aluno estiver ativo" do
+                expect(aluno.aluno_status?).to eq(true)
             end
         end
     end
